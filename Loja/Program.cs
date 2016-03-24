5908c0cdf61bfafd06288439ad93dd826d9b76d5
thlemos <thlemos@gmail.com>
2016-03-24 16:05:02 -0300

    Criando Primeira Tabela

--- Loja/Program.cs
+++ Loja/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Loja.Infra;
 
 namespace Loja
 {
@@ -9,6 +6,7 @@ namespace Loja
     {
         static void Main(string[] args)
         {
+            NHibernateHelper.GeraSchema();
         }
     }
 }
