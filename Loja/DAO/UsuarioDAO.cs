1ff4486e97b0f551a17d5cc4c574a4a858a49216
thlemos <thlemos@gmail.com>
2016-03-24 16:49:38 -0300

    realizando primeiras operações de crud (ainda usando console)

---
+++ Loja/DAO/UsuarioDAO.cs
@@ -0,0 +1,31 @@
+﻿using Loja.Entidades;
+using Loja.Infra;
+using NHibernate;
+
+namespace Loja.DAO
+{
+    public class UsuarioDAO
+    {
+        private ISession session;
+        public UsuarioDAO()
+        {
+            this.session = NHibernateHelper.AbreSession();
+        }
+
+
+        public UsuarioDAO(ISession session)
+        {
+            this.session = session;
+        }
+
+
+        public void Adiciona(Usuario usuario)
+        {
+           
+
+            ITransaction transacao = session.BeginTransaction();
+            session.Save(usuario);
+            transacao.Commit();
+        } 
+    }
+}
--- Loja/Program.cs
+++ Loja/Program.cs
@@ -1,4 +1,8 @@
-﻿using Loja.Infra;
+﻿using System;
+using Loja.DAO;
+using Loja.Entidades;
+using Loja.Infra;
+using NHibernate;
 
 namespace Loja
 {
@@ -6,7 +10,40 @@ namespace Loja
     {
         static void Main(string[] args)
         {
-            NHibernateHelper.GeraSchema();
+            //NHibernateHelper.GeraSchema();
+
+
+
+            //Usuario usuario = new Usuario();
+            //usuario.Nome = "TH";
+
+            //ISession session = NHibernateHelper.AbreSession();
+            //UsuarioDAO usuariosDAO = new UsuarioDAO(session);
+            //usuariosDAO.Adiciona(usuario);
+
+            //Console.Read();
+
+            #region Alterando
+
+            //ISession session = NHibernateHelper.AbreSession();
+            //ITransaction Transacao = session.BeginTransaction();
+            //// Nesse ponto o Usuario tem a propriedade nome com o valor Victor.
+            //Usuario UsuarioDoBanco = session.Get<Usuario>(1);
+
+            //UsuarioDoBanco.Nome = "Victor Harada";
+            //Console.WriteLine("No commit, o NHibernate detecta que o Usuario foi modificado e " +
+            //                  "executa um Update no banco de dados");
+            //Transacao.Commit();
+            //Console.Read();
+
+            #endregion
+
+            ISession session = NHibernateHelper.AbreSession();
+            ITransaction transacao = session.BeginTransaction();
+            Usuario usuario = session.Get<Usuario>(1);
+
+            session.Delete(usuario);
+            transacao.Commit();
         }
     }
 }
